e22df6f2265987b130af7383dd9c0b205c861580
Xavier <xavier.munoz.pisco@gmail.com>
2023-03-25 21:40:36 -0500

    Creando controladores

---
+++ src/main/java/com/cinema/cinema/controllers/MovieControllers.java
@@ -0,0 +1,25 @@
+package com.cinema.cinema.controllers;
+
+import com.cinema.cinema.entities.Movie;
+import com.cinema.cinema.services.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieControllers {
+    private final MovieService movieService;
+
+    public MovieControllers(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping
+    public Movie createMovie(@RequestBody Movie movie){
+        return movieService.createMovie(movie);
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Integer id){
+        return  movieService.getMovieById(id);
+    }
+}
