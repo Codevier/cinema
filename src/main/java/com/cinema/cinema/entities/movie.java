1951c7a35e6fe583baff36bf6c850482eb7593bf
Xavier <xavier.munoz.pisco@gmail.com>
2023-03-25 21:26:36 -0500

    Coneccion lista con MySQL

---
+++ src/main/java/com/cinema/cinema/entities/movie.java
@@ -0,0 +1,21 @@
+package com.cinema.cinema.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+@Entity
+@Table(name="movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private Date duracion;
+}
